0c48eb161bf838128bb58fe7475801efdf00dbab
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-08-22 23:30:51 +0700

    wip

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,6 @@
 import { LatestOrder } from "./(partials)/latest-order"
 import { Stats } from "./(partials)/stats"
 import { TotalRevenue } from "./(partials)/total-revenue"
-import { Overview } from "@/app/(dashboard)/(partials)/overview";
 
 export default function Page() {
   return (
