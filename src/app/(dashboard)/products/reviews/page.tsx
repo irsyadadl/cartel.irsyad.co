693fc518f0ed625608b6fc50b3f8324011ef65bb
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-08-24 20:05:44 +0700

    wip

--- src/app/(dashboard)/products/reviews/page.tsx
+++ src/app/(dashboard)/products/reviews/page.tsx
@@ -1,8 +1,26 @@
 import { Client } from "./client"
+import {
+  SectionContent,
+  SectionDescription,
+  SectionHeader,
+  SectionTitle,
+} from "@/components/section-header"
 
 export const metadata = {
   title: "Product reviews",
 }
 export default function Page() {
-  return <Client />
+  return (
+    <>
+      <SectionHeader>
+        <SectionContent>
+          <SectionTitle>Product reviews</SectionTitle>
+          <SectionDescription>
+            Manage and moderate customer reviews for your products.
+          </SectionDescription>
+        </SectionContent>
+      </SectionHeader>
+      <Client />
+    </>
+  )
 }
